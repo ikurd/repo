d6e7717ff435f8dc9e5794c57afd6decb08aa978
ikurd <Amanjnajat12@hotmail.com>
2019-04-10 19:51:10 +0300

    files

---
+++ cydown.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Cydown</title>
+	<link rel="stylesheet" href="css/style.css" >
+	<link rel="stylesheet" href="css/bootstrap.min.css" >
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css" >
+</head>
+<body class="body" style="background-color:#EFEFF4;">
+  <div class="container">
+  <br />
+  <center>
+  <img src="img/ikurd.png" width="80%">
+  </center>
+    <br />
+  <div class="alert alert-success" role="alert" style="text-align:center;font-size:16px;font-family:'KurdFont';">
+ئە‌م پاكێجە‌ گونجاوە‌ لە‌گە‌ڵ وە‌شانی مۆبایلە‌كە‌ت ☺️
+</div>
+<div class="panel panel-default">
+  <div class="panel-heading"> <h3 class="panel-title" style="font-family: 'KurdFont'"> زانــیــاری </h3></div>
+
+    <li class="list-group-item">ناوی <span style="float:left;color:grey;">Cydown</span></li>
+     <li class="list-group-item">وە‌شان <span style="float:left;color:grey;">6.9.6</span></li>
+     <li class="list-group-item">دوا نوێكردنە‌وە‌ <span style="float:left;color:grey;"><?php date('d / m / y h:i:s A'); ?></span></li>
+    <li class="list-group-item">گونجاوە‌ڵە‌گە‌ڵ <span style="float:left;color:grey;">11.x 10.x 9.x</span></li>
+ <li class="list-group-item">داگرتنە‌كان <span style="float:left;color:grey;">123</span></li>
+</div>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title" style="font-family: 'KurdFont'">دە‌ربارە‌ی پاكێج</h3>
+  </div>
+  <div class="panel-body">تولێکی نایەب بۆ دابەزاندنی تویکە بە پارەکانی سیدیا وە زۆر فەرمانی تر</div>
+</div>
+<a href="#" class="btn btn-primary">ئە‌م پاكێجە‌م بە‌دڵە <span class="glyphicon glyphicon-thumbs-up"></span>‌</a>
+<a href="#" class="btn btn-danger">ئە‌م پاكێجە‌م بە‌ دڵ نیە‌ <span class="glyphicon glyphicon-thumbs-down"></span>‌</a>
+<br/><br/>
+ <a href="screen.html" target="_blank"><li class="list-group-item" style="color:green;">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+   <span class="glyphicon glyphicon-picture"style="color:black;"></span> بینینی وێنە‌ی شاشە‌كان
+  </li>
+  </a>
+<br />
+  <li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+   <span class="glyphicon glyphicon-star"></span> نوێترین پاكێجە‌كان
+  </li>
+
+  <li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+   <span class="glyphicon glyphicon-save"></span> زۆرترین داگیراوە‌كان
+  </li>
+
+ <li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+ <span class="glyphicon glyphicon-stats"></span> باشترین پاكێجە‌كان
+
+  </li>
+<br>
+<li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+<span><img src="img/CydiaIcon.png" width="17px"></span> ماڵپە‌ری سە‌رە‌كی
+
+  </li>
+<li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+ <span><img src="https://s15.postimg.cc/vaory7gqz/10_BA6_E38-3217-4692-_A137-_EAE1_F05_A02_B2.png" width="13px"></span> ئێمە‌ لە‌ فیسبووك
+
+  </li>
+<li class="list-group-item">
+    <span style="float:left;"><img src="img/back-filled.png" width="13px"></span>
+ <span class="glyphicon glyphicon-phone-alt"></span> پە‌یوە‌ندی
+
+  </li>
+<br />
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title" style="font-family: 'KurdFont'">لـێـدوان</h3>
+  </div>
+  <div class="panel-body">
+  <div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://ikurdrepo.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+  </div>
+</div>
+<br />
+<p style="font-size:11px;text-align:center;color:grey;">
+
+میوانی سە‌رھێڵ : 15
+<br />
+كۆی ھە‌موو پاكێجە‌كان : 256
+<br />
+Copyright © iKurd  2017 - 2018
+<br />
+Power By Amanj Xoshanw
+
+</p>
+</div>
+
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+<?php
+
+
+?>
---
+++ download.php
@@ -0,0 +1,3 @@
+<?php
+header('location: com.ikurd.bettersettings_0.0.7_iphoneos-arm.deb');
+ ?>
